affc5c2e40a282b7ec740fd851b632ed446d96c2
AndronikusGameTech <andronikus1335@gmail.com>
2024-06-24 18:57:06 -0400

    That easy. I have a connection working. I probably need to actually figure out how to use environment properties to route in the URL.

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ const port = 3000
 app.use(bodyParser.json());
 app.use(cors())
 
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/SwimSetPlanner", { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)});
+
 const swimSetRoute = require('./routes/SwimSet.route');
 app.use('/swimsets', swimSetRoute);
 app.get('/', (request, response) => {
     response.send(
-        'Endpoint roster:<br/>' + 
+        "Endpoint roster:<br/>" + 
         "/swimsets : Get swim sets<br/>" + 
         "/scheduledsets : Dates on which swim sets were swam."
     )
--- routes/SwimSet.route.js
+++ routes/SwimSet.route.js
@@ -5,13 +5,14 @@ const swimSetRoutes = express.Router();
 let SwimSet = require("../model/SwimSet")
 
 swimSetRoutes.route('/').get(function (request, response) {
-    SwimSet.find(function (error, swimSets) {
-        if (error) {
-            response.status(500).send({'status': 'failure','mssg': 'Internal server error.'});
-        } else {
-            response.status(200).json({'status': 'success','users': swimSets});
-        }
-    });
+    SwimSet.find()
+        .then((results) => {
+            response.status(200).json(results)
+        })
+        .catch((error) => {
+            console.error("Failed to lookup swim sets.", error)
+            response.status(500).json({"message": "Internal server error."})
+        });
 });
 
 module.exports = swimSetRoutes;
