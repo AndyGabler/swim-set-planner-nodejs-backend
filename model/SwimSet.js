648f069ea949343e7ec00a6ffec63497c163b4bd
AndyGaber <andrewpgabler@gmail.com>
2024-07-01 20:07:38 -0400

    Ability to send a POST request to update/create swim sets.

--- model/SwimSet.js
+++ model/SwimSet.js
@@ -3,20 +3,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let SwimSet = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
     repLength: {
-        type: Number
+        type: Number,
+        required: true
     },
     repCount: {
-        type: Number
+        type: Number,
+        required: true
     },
     name: {
-        type: String
+        type: String,
+        required: true
     },
     description: {
         type: String
     },
     labels: {
-        type: Array
+        type: Array,
+        required: true
     }
 },{
     collection: 'SwimSet'
--- routes/SwimSet.route.js
+++ routes/SwimSet.route.js
@@ -4,15 +4,52 @@ const swimSetRoutes = express.Router();
 
 let SwimSet = require("../model/SwimSet")
 
+// GET route for all swim sets
 swimSetRoutes.route('/').get(function (request, response) {
     SwimSet.find()
-        .then((results) => {
+        .then(results => {
             response.status(200).json(results)
         })
-        .catch((error) => {
+        .catch(error => {
             console.error("Failed to lookup swim sets.", error)
             response.status(500).json({"message": "Internal server error."})
         });
+});
+
+// POST route for updates and insertions
+swimSetRoutes.route('/').post(function (request, response) {
+    let inputSwimSet = new SwimSet(request.body)
+    SwimSet.findOne({ 'id': inputSwimSet.id })
+        .then(existingSet => {
+            let updateSet = existingSet
+            term = "update"
+            if (!updateSet) {
+                updateSet = inputSwimSet
+                term = "create"
+            }
+            updateSet.name = inputSwimSet.name || updateSet.name
+            updateSet.repLength = inputSwimSet.repLength || updateSet.repLength
+            updateSet.repCount = inputSwimSet.repCount || updateSet.repCount
+            updateSet.description = inputSwimSet.description || updateSet.description
+            updateSet.labels = inputSwimSet.labels || updateSet.labels
+
+            updateSet.save()
+                .then(result => {
+                    console.log("Successfully performed " + term + " for swim set with ID " + result.id)
+                    response.status(200).json({
+                        "message": term + " performed successfully",
+                        "swimSet": result
+                    })
+                })
+                .catch(error => {
+                    console.error("Set " + term + " failed", error)
+                    response.status(500).json({ "message": "Internal server error blocked " + term })
+                })
+        })
+        .catch(error => {
+            console.error("Lookup for existing set failed.", error)
+            response.status(500).json({ "message": "Internal server error." })
+        })
 });
 
 module.exports = swimSetRoutes;
